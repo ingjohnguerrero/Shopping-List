d9ac03b4d20f0cc33abffe889f042672937e54eb
John Guerrero <ingjohnguerrero@gmail.com>
2019-12-08 20:45:03 -0500

    Initial Commit

---
+++ Shopping List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopping List
+//
+//  Created by John Edwin Guerrero Ayala on 8/12/19.
+//  Copyright © 2019 John Edwin Guerrero Ayala. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
